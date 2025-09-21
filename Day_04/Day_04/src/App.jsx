2e05e4cd95c03fd615d9eb543d87848dddad3f5a
im-inferno <shadowinferno17@gmail.com>
2025-09-21 19:08:45 +0530

    Day 01 of project currency convertor, desinged the input  card

---
+++ Day_04/Day_04/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat bg-[url("https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80")]'>
+    
+    </div>
+  )
+}
+
+export default App
